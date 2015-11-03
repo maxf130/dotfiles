b96824f662bcfa4fdac443d46b07dab6314f9a32
Maximilian Friedersdorff <maxf130@gmail.com>
2015-11-03 17:59:40 +0000

    Remove echo statements from makelinks.

--- makelinks.sh
+++ makelinks.sh
@@ -13,21 +13,15 @@ files="vimrc bashrc xsession Xresources gitconfig zprofile zshenv zshrc xinitrc"
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, 
 #then create symlinks from the homedir to any files in the 
 #~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file $olddir/
-    echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
